1ff0665ff49bc9d2f91f158e0b84aec7a3a25009
AritraLikhan <aritralikhan2001@gmail.com>
2024-10-11 20:17:29 +0600

    gitignore updated

--- src/main/java/com/hoshino/ruby/bookmarker_api/BookmarkerApiApplication.java
+++ src/main/java/com/hoshino/ruby/bookmarker_api/BookmarkerApiApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BookmarkerApiApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(BookmarkerApiApplication.class, args);
+		
 	}
 
 }
